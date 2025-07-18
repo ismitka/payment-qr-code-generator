828480faa2925c2157b0e0d245289d041ba4b73e
Ivan Smitka <ivan@smitka.net>
2025-07-18 10:53:13 +0200

    1.1

--- compress.sh
+++ compress.sh
@@ -1,11 +1,12 @@
 sass scss/default.scss static/css/default.css
+rm -rf lib/phpqrcode/cache/*
 (cd ../ && zip -r payment-qr-code-generator/payment-qr-code-generator.zip \
   payment-qr-code-generator/data/data.json.template \
   payment-qr-code-generator/lib \
   payment-qr-code-generator/static \
-  wp-time/LICENSE \
-  wp-time/README.md \
-  wp-time/api.php \
-  wp-time/generator.php \
-  wp-time/payment-qr-code-generator.php \
+  payment-qr-code-generator/LICENSE \
+  payment-qr-code-generator/README.md \
+  payment-qr-code-generator/api.php \
+  payment-qr-code-generator/generator.php \
+  payment-qr-code-generator/payment-qr-code-generator.php \
   )
