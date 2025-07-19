9ed28f6e85d18ba051d8472e8b8de19ad1205861
Ivan Smitka <ivan@smitka.net>
2025-07-19 16:57:30 +0200

    1.1

--- compress.sh
+++ compress.sh
@@ -1,6 +1,10 @@
-sass scss/default.scss static/css/default.css
+#!/bin/bash
 rm -rf lib/phpqrcode/cache/*
-(cd ../ && zip -r payment-qr-code-generator/payment-qr-code-generator.zip \
+sass scss/default.scss static/css/default.css
+VERSION=$(git tag -l)
+echo "${VERSION}"
+sed -i -E "s/^ \* Version\: .*$/ * Version: ${VERSION}/g" payment-qr-code-generator.php
+(cd ../ && zip -r payment-qr-code-generator/${VERSION}.zip \
   payment-qr-code-generator/data/data.json.template \
   payment-qr-code-generator/lib \
   payment-qr-code-generator/static \
